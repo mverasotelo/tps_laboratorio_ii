acb0fff23c4dcf8199708dc88834b9c96680b75f
mverasotelo <mer-vs@hotmail.com>
2021-09-15 10:36:19 -0300

    entrega TP

---
+++ TP1_ProgII-VeraSotelo,Mercedes/Entidades/Operando.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        //atributo
+        private double numero;
+
+        private string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        //constructores
+        public Operando()
+        {
+            numero = 0;
+        }
+
+        public Operando(double numero)
+            : this()
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+            : this()
+        {
+            this.Numero  = strNumero;
+        }
+
+        //metodos
+        /// <summary>
+        /// Valida que un operando ingresado como string sea un numero y lo devuelve como double
+        /// </summary>
+        /// <param name="strNumero">cadena de caracteres a validar</param>
+        /// <returns>devuelve el operando en double si se valida, o 0 sino se puede validar </returns>
+        private double ValidarOperando(string strNumero)
+        {
+            if (double.TryParse(strNumero, out double numero))
+            {
+                return numero;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Verifica que un numero sea binario (que contenga solo 0 y 1)
+        /// </summary>
+        /// <param name="binario">cadena de caracteres a chequear</param>
+        /// <returns>devuelve true si es binario y false sino lo es</returns>
+        private bool EsBinario(string binario)
+        {
+            foreach (char caracter in binario)
+            {
+                if (caracter != '0' && caracter != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Convierte un numero entero binario al sistema decimal
+        /// </summary>
+        /// <param name="binario">numero entero binario a convertir</param>
+        /// <returns>El numero en decimal</returns>
+        public string BinarioDecimal(string binario)
+        {
+            int numeroDecimal = 0;
+ 
+            if (EsBinario(binario))
+            {
+                char[] binarioCharArray = binario.ToCharArray();
+                Array.Reverse(binarioCharArray);
+
+                for (int i = 0; i < binarioCharArray.Length; i++)
+                {
+                    if (binarioCharArray[i] == '1')
+                    {
+                        if (i == 0)
+                        {
+                            numeroDecimal += 1;
+                        }
+                        else
+                        {
+                            numeroDecimal += (int) Math.Pow(2, i);
+                        }
+                    }
+                }
+                return numeroDecimal.ToString();
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            int numeroEntero = (int) numero;
+
+            if (numeroEntero >= 0)
+            {
+                string binario = "";
+                while (numeroEntero > 0)
+                {
+                    if (numeroEntero % 2 == 0)
+                    {
+                        binario = "0" + binario;
+                    }
+                    else
+                    {
+                        binario = "1" + binario;
+                    }
+                    numeroEntero = numeroEntero / 2;
+                }
+                return binario;
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            if(double.TryParse(numero, out double num))
+            {
+                return DecimalBinario(num);
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+
+        //sobrecarga de operadores
+
+        /// <summary>
+        /// Realiza la suma entre 2 numeros pasados por parametro
+        /// </summary>
+        /// <param name="n1">primer operando</param>
+        /// <param name="n2">segundo operando</param>
+        /// <returns>resultado</returns>
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        /// <summary>
+        /// Realiza la resta entre 2 numeros pasados por parametro
+        /// </summary>
+        /// <param name="n1">primer operando</param>
+        /// <param name="n2">segundo operando</param>
+        /// <returns>resultado</returns>
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        /// <summary>
+        /// Realiza la multiplicacion de 2 numeros pasados por parametro
+        /// </summary>
+        /// <param name="n1">primer operando</param>
+        /// <param name="n2">segundo operando</param>
+        /// <returns>resultado</returns>
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        /// <summary>
+        /// Realiza la division de 2 numeros pasados por parametro
+        /// </summary>
+        /// <param name="n1">primer operando</param>
+        /// <param name="n2">segundo operando</param>
+        /// <returns>si el segundo operando es distinto de 0 devuelve resultado, si es 0 devuelve double.MinValue</returns>
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if (n2.numero == 0)
+            {
+                return double.MinValue;
+            }
+            return n1.numero / n2.numero;
+        }
+    }
+}
---
+++ TP1_ProgII-VeraSotelo,Mercedes/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            Limpiar(Controls);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar(Controls);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("¿Seguro de querer salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            string numero1 = txtNumero1.Text;
+            string numero2 = txtNumero2.Text;
+            string operador = cmbOperador.Text;
+            double resultado = Operar(numero1, numero2, operador);
+            lblResultado.Text = resultado.ToString();
+
+            if (numero1 == "")
+            {
+                numero1 = "0";
+            }
+            if (numero2 == "")
+            {
+                numero2 = "0";
+            }
+            if (operador == " ")
+            {
+                operador = "+";
+            }
+
+            lstOperaciones.Items.Add($"{numero1}{operador}{numero2}= {resultado}");
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            if (lblResultado.Text != "")
+            {
+                    Operando resultado = new Operando(lblResultado.Text);
+                    lblResultado.Text = resultado.DecimalBinario(lblResultado.Text);
+            }
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            if (lblResultado.Text != "")
+            {
+                Operando resultado = new Operando(lblResultado.Text);
+                lblResultado.Text = resultado.BinarioDecimal(lblResultado.Text);
+            }
+        }
+
+        private void Limpiar(Control.ControlCollection campos)
+        {
+            foreach (Control campo in campos)
+            {
+                if (campo is TextBox)
+                {
+                    ((TextBox)campo).Clear();
+                }
+                else if (campo is ComboBox)
+                {
+                    ((ComboBox)campo).SelectedIndex = 0;
+                }
+                else if (campo is Label)
+                {
+                    campo.Text = "0";
+                }
+            }
+        }
+
+        public static double Operar(string numero1, string numero2, string operador)
+        {
+            Calculadora calculadora = new Calculadora();
+            Operando operando1 = new Operando(numero1);
+            Operando operando2 = new Operando(numero2);
+            char operadorChar = char.Parse(operador);
+            return calculadora.Operar(operando1, operando2, operadorChar);
+        }
+    }
+}
